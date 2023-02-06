92a009b1dcd472fbc17956119748a9f9ba23e89d
mmeising <mmeising@student.42heilbronn.de>
2023-02-06 21:35:39 +0100

    actually create user every fresh volume install and skip email sending

--- srcs/requirements/wordpress/entry.sh
+++ srcs/requirements/wordpress/entry.sh
@@ -2,9 +2,8 @@
 
 wp core install --url=${DOMAIN} --title=${TITLE} --admin_user=${WP_ROOT_USER} \
                 --admin_password=${WP_ROOT_PWD} --admin_email=${WP_ROOT_MAIL} \
-                --allow-root
+                --skip-email --allow-root                                     \
 
-export root=$(wp user list | grep dieter)
-export max=$(wp user list | grep max)
+wp user create ${WP_USER} ${WP_MAIL} --user_pass=${WP_PWD}
 
 php-fpm8 -F
